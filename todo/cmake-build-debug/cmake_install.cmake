ef0513d43cd42852ba7536f0989c3b7762ad36d6
91bcsabi <c.balazsi91@gmail.com>
2019-10-03 15:06:29 +0200

    todo

--- todo/cmake-build-debug/cmake_install.cmake
+++ todo/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/Csabi/Desktop/csabigit/greenfox/91bcsabi/91bcsabi/week-4/day-4/bcsabi-Todo-app/todo
+# Install script for directory: /Users/Csabi/Desktop/Todo app/bcsabi-Todo-app/todo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/Csabi/Desktop/csabigit/greenfox/91bcsabi/91bcsabi/week-4/day-4/bcsabi-Todo-app/todo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/Csabi/Desktop/Todo app/bcsabi-Todo-app/todo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- todo/main.cpp
+++ todo/main.cpp
@@ -13,14 +13,8 @@ void printUsage() {
     std::cout << "      -c   Completes an task" << std::endl;
 }
 
-void addTask(std::string newTask){
-    std::ofstream myFile;
-    myFile.open("tasks.txt", std::ios::app);
-    myFile << "\n" + newTask;
-    myFile.close();
-}
 
-void listTasks() {
+void listTasks(){
     std::ifstream myFile;
     myFile.open("tasks.txt");
     std::string text;
@@ -34,28 +28,40 @@ void listTasks() {
     }
 }
 
+void addTask(std::string newTask){
+    std::ofstream myFile;
+    myFile.open("tasks.txt", std::ios::app);
+    myFile << "\n" + newTask;
+    myFile.close();
+}
+
+void removeTasks(int position){
+    std::ifstream myFile;
+    myFile.open("tasks.txt");
+    position = line.find(deleteLine);
+    if (position != string::npos) {
+        line.replace(line.find(deleteLine), deleteLine.length(), "");
+    }
+    myFile.close();
+}
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         printUsage();
-    } else if (std::string(argv[1]) == ("-l")) {
+    }else if (std::string(argv[1]) == ("-l")) {
         listTasks();
-    }
-    else if(std::string(argv[1]) == ("-a")){
+    }else if(std::string(argv[1]) == ("-a")){
         if (argc == 2){
                 std::cout << "not written anything after -a .Please write something" << std::endl;
         }else{
             addTask(argv[2]);
             }
+    }else if(std::string(argv[1]) == ("-r")){
+        if (argc == 2) {
+            std::cout << "not written anything after -r .Please write which line you want to remove" << std::endl;
+        }else{
+            removeTasks(argv[2]);
         }
-    /*
-}else if(std::string(argv[1]) == ("-a")){
-    //add...
-}else if(std::string(argv[1]) == ("-r")){
-    //remove...
-}else if(std::string(argv[1]) == ("c")){
-    //completes tasks.txt
-}
-}
-*/
+    }
     return 0;
 }
